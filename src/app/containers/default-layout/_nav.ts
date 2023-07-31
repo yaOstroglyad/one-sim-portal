a7eb6043884132aa7f650b2f509ce7e49483d27a
andreyostroglyad <andrey@triber.app>
2023-07-31 20:43:30 +0300

    - first commit

---
+++ src/app/containers/default-layout/_nav.ts
@@ -0,0 +1,9 @@
+import { INavData } from '@coreui/angular';
+
+export const navItems: INavData[] = [
+  {
+    name: 'Form Control',
+    url: '/forms/form-control',
+    iconComponent: { name: 'cil-speedometer' },
+  }
+];
