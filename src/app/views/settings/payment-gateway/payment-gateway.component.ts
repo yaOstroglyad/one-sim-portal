8f5b55df4492c3681afbf00a41f8881ee480ecd5
andreyostroglyad <andrey@triber.app>
2024-10-27 17:09:45 +0200

    - form fix

--- src/app/views/settings/payment-gateway/payment-gateway.component.ts
+++ src/app/views/settings/payment-gateway/payment-gateway.component.ts
@@ -1,6 +1,6 @@
-import { ChangeDetectorRef, Component, inject } from '@angular/core';
+import { ChangeDetectorRef, Component } from '@angular/core';
 import { PaymentGatewayService } from './payment-gateway.service';
-import { forkJoin, combineLatest } from 'rxjs';
+import { forkJoin, combineLatest, of } from 'rxjs';
 import { switchMap, map } from 'rxjs/operators';
 import { PaymentGatewayUtilsService } from './payment-gateway.utils.service';
 import { PgComponentConfig } from '../../../shared/model/payment-strategies';
@@ -22,6 +22,7 @@ export class PaymentGatewayComponent {
 		private cd: ChangeDetectorRef
 	) {
 		this.isAdmin = this.getAdmin();
+
 		combineLatest([
 			this.paymentGatewayService.list(),
 			this.paymentGatewayService.getPaymentStrategyTypes()
@@ -29,20 +30,18 @@ export class PaymentGatewayComponent {
 			switchMap(([metadata, types]) => {
 				if (!types || types.length === 0) {
 					console.error('No types found');
+					return of([]);
 				}
 
 				const requests = types.map(type => {
 					const strategyMetadata = metadata.find(meta => meta.paymentStrategy === type);
 
-					return forkJoin({
-						fields: this.paymentGatewayService.getFieldsByStrategyType(type)
-					}).pipe(
-						map(({ fields }) => {
+					return this.paymentGatewayService.getFieldsByStrategyType(type).pipe(
+						map(fields => {
 							const paymentMethodParameters = strategyMetadata ? strategyMetadata.paymentMethodParameters : {};
-
 							return {
 								id: strategyMetadata?.id,
-								isActive: strategyMetadata.isActive,
+								isActive: strategyMetadata?.isActive,
 								type,
 								config: this.paymentGatewayUtilsService.generateForm(fields, paymentMethodParameters)
 							};
