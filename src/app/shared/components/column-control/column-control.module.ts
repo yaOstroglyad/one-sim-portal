64a1408dbd16c454a546212952aadb7bee7bccd8
andreyostroglyad <andrey@triber.app>
2025-08-09 21:12:22 +0300

    - column control view modified

--- src/app/shared/components/column-control/column-control.module.ts
+++ src/app/shared/components/column-control/column-control.module.ts
@@ -1,14 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ColumnControlComponent } from './column-control.component';
-import {
-	ButtonDirective,
-	DropdownComponent,
-	DropdownItemDirective,
-	DropdownMenuDirective,
-	DropdownToggleDirective, FormCheckComponent, FormCheckInputDirective, FormCheckLabelDirective
-} from '@coreui/angular';
 import { TranslateModule } from '@ngx-translate/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatIconModule } from '@angular/material/icon';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatTooltipModule } from '@angular/material/tooltip';
+import { MatDividerModule } from '@angular/material/divider';
+import { ButtonDirective } from '@coreui/angular';
 import { IconDirective } from '@coreui/icons-angular';
 
 
@@ -22,15 +22,14 @@ import { IconDirective } from '@coreui/icons-angular';
 	],
 	imports: [
 		CommonModule,
-		DropdownComponent,
-		ButtonDirective,
-		DropdownToggleDirective,
-		DropdownItemDirective,
-		DropdownMenuDirective,
-		FormCheckComponent,
-		FormCheckLabelDirective,
-		FormCheckInputDirective,
 		TranslateModule,
+		MatButtonModule,
+		MatMenuModule,
+		MatIconModule,
+		MatCheckboxModule,
+		MatTooltipModule,
+		MatDividerModule,
+		ButtonDirective,
 		IconDirective
 	]
 })
